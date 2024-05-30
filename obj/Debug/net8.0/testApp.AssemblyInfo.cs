eebd69cca38d81c1069b4e5eff428b22c8aa7fa6
Cora <115031670+rachelcorawood@users.noreply.github.com>
2024-05-30 13:37:33 -0700

    firstCommit

--- obj/Debug/net8.0/testApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/testApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c700af3108d35a9af5c342754be1b09df41acc")]
 [assembly: System.Reflection.AssemblyProductAttribute("testApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
